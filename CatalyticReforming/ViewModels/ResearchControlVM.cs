ad575665645156e5ab1cd842a899f1c5e3fd86b2
Артемий Нерадовский <reqieskorigin@gmail.com>
2023-05-03 21:44:35 +0300

    Дописал свойства

--- CatalyticReforming/ViewModels/ResearchControlVM.cs
+++ CatalyticReforming/ViewModels/ResearchControlVM.cs
@@ -9,6 +9,16 @@ public class ResearchControlVM : ViewModelBase
     private NavigationService _navigationService;
 
     public ObservableCollection<string> InsallersCollection { get; set; }
+    public ObservableCollection<string> ReactorsCollection { get; set; }
+    public ObservableCollection<double> ReactorPressure { get; set; }
+    public ObservableCollection<string> CatalystCollection { get; set; }
+    public ObservableCollection<double> CatalystDensity { get; set; }
+    public ObservableCollection<double> StrengthFactor { get; set; }
+    public ObservableCollection<string> MaterialCollection { get; set; }
+    public ObservableCollection<double> NaphthenicHydrocarbons { get; set; }
+    public ObservableCollection<double> AromaticHydrocarbons { get; set; }
+    public ObservableCollection<double> MaterialsInput { get; set; }
+    public ObservableCollection<double> Temperature { get; set; }
 
     private RelayCommand _startResearchCommand;
     public ResearchControlVM(NavigationService navigationService)
