c2cbf4217ac5eb4a1f1d9458710fe92ff1ac4df0
Артемий Нерадовский <reqieskorigin@gmail.com>
2023-05-03 23:36:25 +0300

    Сделал поиск пути матлаба по регистру

--- CatalyticReforming/ViewModels/ResearchControlVM.cs
+++ CatalyticReforming/ViewModels/ResearchControlVM.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
 using CatalyticReforming.Commands;
 using CatalyticReforming.Services;
+using Microsoft.Win32;
 
 namespace CatalyticReforming.ViewModels;
 
@@ -32,8 +34,39 @@ public class ResearchControlVM : ViewModelBase
         {
             return _startResearchCommand ??= new RelayCommand(o =>
             {
-                
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                startInfo.FileName = GetInstallationPath("MATLAB") + "/bin/Matlab.exe"; // путь к исполняемому файлу MATLAB
+                startInfo.Arguments = "\"x = -10:0.1:10; y = x.^2; plot(x,y);\""; // код для выполнения
+                Process.Start(startInfo);
             });
         }
+    }
+    string GetInstallationPath(string programName)
+    {
+        // Открываем нужную ветку реестра
+        using (RegistryKey? key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"))
+        {
+            // Проходим по всем ключам в ветке Uninstall
+            foreach (string subKeyName in key.GetSubKeyNames())
+            {
+                using (RegistryKey subKey = key.OpenSubKey(subKeyName))
+                {
+                    // Получаем значение DisplayName и проверяем, соответствует ли оно искомой программе
+                    object displayNameValue = subKey.GetValue("DisplayName");
+                    if (displayNameValue != null && displayNameValue.ToString().Contains(programName))
+                    {
+                        // Если нашли нужную программу, то получаем значение InstallLocation и возвращаем его
+                        object installLocationValue = subKey.GetValue("InstallLocation");
+                        if (installLocationValue != null)
+                        {
+                            return installLocationValue.ToString();
+                        }
+                    }
+                }
+            }
+        }
+
+        // Если не нашли программу, то возвращаем пустую строку
+        return string.Empty;
     }
 }
