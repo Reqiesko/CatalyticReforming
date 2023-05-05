9811b78c8f21fc643b7ad79e78e70d8d41f79506
Артемий Нерадовский <reqieskorigin@gmail.com>
2023-05-05 22:45:12 +0300

    Отправляю на гитхаб чтоб Рома посмотрел у себя

--- CatalyticReforming/Views/ResearchControlVM.cs
+++ CatalyticReforming/Views/ResearchControlVM.cs
@@ -17,31 +17,17 @@ namespace CatalyticReforming.Views;
 public class ResearchControlVM : ViewModelBase
 {
     private NavigationService _navigationService;
-
-    private RelayCommand _startResearchCommand;
     
-    public ResearchControlVM(NavigationService navigationService)
-    {
-        _navigationService = navigationService;
-        MatlabCode = "% Создание таблицы/n" +
-                     "T = 490:1:503; % диапазон температур" +
-                     "/nG = 100:100:1000; % диапазон потока сырья/n" +
-                     "[TT, GG] = meshgrid(T, G);/n" +
-                     "yn = 0.1; % фиксированное значение/n" +
-                     "ya = 0.2; % фиксированное значение/n" +
-                     "F = targetFunction(TT, GG, yn, ya);/n" +
-                     "tableData = [TT(:), GG(:), F(:)];/n" +
-                     "tableHeaders = {'T', 'G', 'F'};/n" +
-                     "tableResult = array2table(tableData, 'VariableNames', tableHeaders)/n" +
-                     "% Построение графика/n" +
-                     "figure;/n" +
-                     "surf(TT, GG, F);/n" +
-                     "title('Целевая функция');/n" +
-                     "xlabel('T');/n" +
-                     "ylabel('G');/n" +
-                     "zlabel('F');";
-    }
-
+    private RelayCommand _startResearchCommand;
+    private RelayCommand _changeTemperatureCommand;
+    private RelayCommand _changeMaterialCommand;
+    private RelayCommand _materialCheckChangeCommand;
+    private RelayCommand _temperatureCheckChangeCommand;
+    private bool TemperatureCheckBox { get; set; }
+    private bool MaterialCheckBox { get; set; }
+    public string MatlabCode { get; set; }
+    
+    
     public ObservableCollection<string> InsallersCollection { get; set; }
     public ObservableCollection<string> ReactorsCollection { get; set; }
     public ObservableCollection<double> ReactorPressure { get; set; }
@@ -51,10 +37,45 @@ public class ResearchControlVM : ViewModelBase
     public ObservableCollection<string> MaterialCollection { get; set; }
     public ObservableCollection<double> NaphthenicHydrocarbons { get; set; }
     public ObservableCollection<double> AromaticHydrocarbons { get; set; }
-    public ObservableCollection<double> MaterialsInput { get; set; }
-    public ObservableCollection<double> Temperature { get; set; }
+    public string MaterialsInput { get; set; }
+    public string Temperature { get; set; }
     
-    public string MatlabCode { get; set; }
+    public ResearchControlVM(NavigationService navigationService)
+    {
+        _navigationService = navigationService;
+        MaterialCheckBox = false;
+        TemperatureCheckBox = false;
+        
+        MatlabCode = $"function [tableResult] = targetFunction()/n" +
+                     "% Определение параметров функции/n" +
+                     "a = 15.802;/n" +
+                     "b = 0.03155;/n" +
+                     "c = 0.95975;/n" +
+                     "d = 2.4206;/n" +
+                     "yn = 0.1;/n" +
+                     "ya = 0.2;/n" +
+                     "% Создание таблицы/n" +
+                     "T = " + Temperature + ";/n" +
+                     "G = "+ MaterialsInput + ";/n" +
+                     "[TT, GG] = meshgrid(T, G);/n" +
+                     "F = a - b * TT + c * GG - d*(yn - ya);/n" +
+                     "tableData = [TT(:), GG(:), F(:)];/n" +
+                     "tableHeaders = {'T', 'G', 'F'};/n" +
+                     "tableResult = array2table(tableData, 'VariableNames', tableHeaders);/n" +
+                     "% Построение графика/n" +
+                     "figure;/n" +
+                     "surf(TT, GG, F);/n" +
+                     "title('Целевая функция');/n" +
+                     "xlabel('T');/n" +
+                     "ylabel('G');/n" +
+                     "zlabel('F');/n" +
+                     "end";
+    }
+
+    //uitable('Data',tableResult{:,:},'ColumnName',tableResult.Properties.VariableNames,'Units','Normalized','Position',[0,0,1,1]);
+    //tableResult = targetFunction();
+    
+    
     public RelayCommand StartResearchCommand
     {
         get
@@ -71,6 +92,115 @@ public class ResearchControlVM : ViewModelBase
         }
     }
 
+
+    public RelayCommand ChangeTemperatureCommand
+    {
+        get
+        {
+            return _changeTemperatureCommand ??= new RelayCommand(o =>
+            {
+                TemperatureCheckBox = TemperatureCheckBox != true;
+                if (TemperatureCheckBox == true)
+                {
+                    Temperature = "490:1:503";
+                    MatlabCode = $"function [tableResult] = targetFunction()/n" +
+                                 "% Определение параметров функции/n" +
+                                 "a = 15.802;/n" +
+                                 "b = 0.03155;/n" +
+                                 "c = 0.95975;/n" +
+                                 "d = 2.4206;/n" +
+                                 "yn = 0.1;/n" +
+                                 "ya = 0.2;/n" +
+                                 "% Создание таблицы/n" +
+                                 "T = " + Temperature + ";/n" +
+                                 "G = "+ MaterialsInput + ";/n" +
+                                 "[TT, GG] = meshgrid(T, G);/n" +
+                                 "F = a - b * TT + c * GG - d*(yn - ya);/n" +
+                                 "tableData = [TT(:), GG(:), F(:)];/n" +
+                                 "tableHeaders = {'T', 'G', 'F'};/n" +
+                                 "tableResult = array2table(tableData, 'VariableNames', tableHeaders);/n" +
+                                 "% Построение графика/n" +
+                                 "figure;/n" +
+                                 "surf(TT, GG, F);/n" +
+                                 "title('Целевая функция');/n" +
+                                 "xlabel('T');/n" +
+                                 "ylabel('G');/n" +
+                                 "zlabel('F');/n" +
+                                 "end";
+                }
+                else
+                {
+                    return;
+                }
+            });
+        }
+    }
+    
+    public RelayCommand ChangeMaterialCommand
+    {
+        get
+        {
+            return _changeMaterialCommand ??= new RelayCommand(o =>
+            {
+                MaterialCheckBox = MaterialCheckBox != true;
+                if (MaterialCheckBox == true)
+                {
+                    MaterialsInput = "8.87:0.1:16.04";
+                    MatlabCode = $"function [tableResult] = targetFunction()/n" +
+                                 "% Определение параметров функции/n" +
+                                 "a = 15.802;/n" +
+                                 "b = 0.03155;/n" +
+                                 "c = 0.95975;/n" +
+                                 "d = 2.4206;/n" +
+                                 "yn = 0.1;/n" +
+                                 "ya = 0.2;/n" +
+                                 "% Создание таблицы/n" +
+                                 "T = " + Temperature + ";/n" +
+                                 "G = "+ MaterialsInput + ";/n" +
+                                 "[TT, GG] = meshgrid(T, G);/n" +
+                                 "F = a - b * TT + c * GG - d*(yn - ya);/n" +
+                                 "tableData = [TT(:), GG(:), F(:)];/n" +
+                                 "tableHeaders = {'T', 'G', 'F'};/n" +
+                                 "tableResult = array2table(tableData, 'VariableNames', tableHeaders);/n" +
+                                 "% Построение графика/n" +
+                                 "figure;/n" +
+                                 "surf(TT, GG, F);/n" +
+                                 "title('Целевая функция');/n" +
+                                 "xlabel('T');/n" +
+                                 "ylabel('G');/n" +
+                                 "zlabel('F');/n" +
+                                 "end";
+                }
+                else
+                {
+                    return;
+                }
+            });
+        }
+    }
+    
+    public RelayCommand TemperatureCheckChangeCommand
+    {
+        get
+        {
+            return _temperatureCheckChangeCommand ??= new RelayCommand(o =>
+            {
+                TemperatureCheckBox = TemperatureCheckBox != true;
+            });
+        }
+    }
+    
+    public RelayCommand MaterialCheckChangeCommand
+    {
+        get
+        {
+            return _materialCheckChangeCommand ??= new RelayCommand(o =>
+            {
+                MaterialCheckBox = MaterialCheckBox != true;
+            });
+        }
+    }
+    
     private string GetInstallationPath(string programName)
     {
         // Открываем нужную ветку реестра
